a12f886bbb7ba4a3b0a8669bc058c42c98c1b5a5
Tengen02 <161907020+Tengen02@users.noreply.github.com>
2026-02-13 14:50:13 +0200

    config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine today🌹", // The big question!
+            text: "Will you be my Valentine 🥺", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,14 +42,14 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "WOOOOW You love me that much?? 🥺💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
+        title: "I'm the luckiest person in the world! 🎉💝💖💝💓",
         message: "Now come get your gift, I love you so much 🥺!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://console.cloudinary.com/app/c-c571a321974c00386fb128192bb602/assets/media_library/search/asset/7c35a069608609fffb2aaf32a5fbac2d/manage?view_mode=mosaic&context=manage", // Music streaming URL
+        musicUrl: "https://collection.cloudinary.com/dldp2xrue/80baa200cd52d1b037d3fe863601e7b8", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
